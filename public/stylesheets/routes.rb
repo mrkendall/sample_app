47c8496b76b363c1e0a0c3751a8a7875ba6fd974
Mike Kendall <kendallm@gmail.com>
2011-08-17 10:39:19 -0700

    Finished layout and routes

--- public/stylesheets/routes.rb
+++ public/stylesheets/routes.rb
@@ -1,13 +1,10 @@
 SampleApp::Application.routes.draw do
 
-
-  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match '/root', :to => 'pages#home'
-  
-  
+
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
